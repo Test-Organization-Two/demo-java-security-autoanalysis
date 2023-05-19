32526375c154c575c2cf87e3878aace6119a069f
robbiebise <118204121+robbiebise@users.noreply.github.com>
2023-05-19 16:15:07 -0500

    Update AppTest.java

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -6,12 +6,15 @@ import static org.junit.Assert.assertNotNull;
 public class AppTest {
 //TODO: something
 //TODO: something
+    public int foo(int a) {
+        int b = 12;
+        if (a == 1) {
+            return b;
+        }
+        return b;  // Noncompliant
+     }
     @Test
     public void testGenerateKey() {
-        int j;
-        while (true) { // Noncompliant; end condition omitted
-            j++;
-        }
         assertNotNull(Utils.generateKey());
     }
 }
